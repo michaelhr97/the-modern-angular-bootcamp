eeb36821d1ebca2e79304b00ab67d92d1bc1ae98
michael <michaelhernandezrala@gmail.com>
2023-11-10 13:13:40 +0100

    Add event binding syntax to the button to generate password

--- 02 - Understanding Angular Template Syntax/password-generator/src/app/app.component.ts
+++ 02 - Understanding Angular Template Syntax/password-generator/src/app/app.component.ts
@@ -7,8 +7,10 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  onButtonClick() {
+    console.log('Button was click');
+  }
 }
