3485098e2155eb3b9001775ea2a13a0711fc5f29
michael <michaelhernandezrala@gmail.com>
2023-11-11 19:49:04 +0100

    Typing data to a component

--- 03 - Building Components/cards/src/app/app.component.ts
+++ 03 - Building Components/cards/src/app/app.component.ts
@@ -11,5 +11,24 @@ import { CardComponent } from './card/card.component';
   imports: [CommonModule, RouterOutlet, CardComponent],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: '@nature',
+      imageUrl: '../assets/tree.jpeg',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: '@mountainlover',
+      imageUrl: '../assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      username: '@biking1222',
+      imageUrl: '../assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
 }
