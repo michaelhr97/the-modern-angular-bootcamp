84053d96a3311e6716eed068f9c875b9890d9c3f
michael <michaelhernandezrala@gmail.com>
2023-11-11 20:57:28 +0100

    Conditional Display

--- 04 - Building a Typing Game/typing/src/app/app.component.ts
+++ 04 - Building a Typing Game/typing/src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { lorem } from 'faker';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
 }
