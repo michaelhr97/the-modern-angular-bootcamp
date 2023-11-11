16848c79d60298abaeab59e155af22c7bc718b82
michael <michaelhernandezrala@gmail.com>
2023-11-11 19:31:45 +0100

    Creating components

--- 03 - Building Components/cards/src/app/app.component.ts
+++ 03 - Building Components/cards/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, CardComponent],
 })
 export class AppComponent {
   title = 'cards';
