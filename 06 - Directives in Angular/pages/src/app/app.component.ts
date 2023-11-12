c5e6fcc46c55cb782c4f35a98b8018b9d7e983b9
michael <michaelhernandezrala@gmail.com>
2023-11-12 22:23:08 +0100

    Getting some data

--- 06 - Directives in Angular/pages/src/app/app.component.ts
+++ 06 - Directives in Angular/pages/src/app/app.component.ts
@@ -7,8 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://unsplash.com/es/fotos/orilla-del-mar-durante-la-hora-dorada-KMn4VEeEPR8',
+    },
+    {
+      title: 'Playing tennis',
+      url: 'https://unsplash.com/es/fotos/hombre-jugando-tenis-WqI-PbYugn4',
+    },
+    {
+      title: 'Coding',
+      url: 'https://unsplash.com/es/fotos/monitor-que-muestra-programacion-java-OqtafYT5kTw',
+    },
+    {
+      title: 'Running',
+      url: 'https://unsplash.com/es/fotos/par-de-zapatillas-adidas-azules-y-blancas-XiZ7pRvCzro',
+    },
+  ];
 }
