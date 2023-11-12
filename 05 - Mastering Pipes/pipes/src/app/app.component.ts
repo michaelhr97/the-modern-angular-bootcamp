77884908b12f7deecedbaa8ce4593b60a1c0445b
michael <michaelhernandezrala@gmail.com>
2023-11-12 21:59:19 +0100

    Creating custom pipes

--- 05 - Mastering Pipes/pipes/src/app/app.component.ts
+++ 05 - Mastering Pipes/pipes/src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, ConvertPipe],
 })
 export class AppComponent {
   name = '';
   date = '';
   amount = '';
   height = 0;
+  miles = 0;
+
   car = {
     make: 'Toyota',
     model: 'Camry',
@@ -34,5 +37,9 @@ export class AppComponent {
 
   onHeightChange(value: string) {
     this.height = Number.parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = Number.parseFloat(value);
   }
 }
