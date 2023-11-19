50f0f9da2a93cf94b9da49ef7179081723bcde33
michael <michaelhernandezrala@gmail.com>
2023-11-19 12:02:33 +0100

    Project setup

--- 08 - Routing and Navigation Between Pages/comps/src/app/app.module.ts
+++ 08 - Routing and Navigation Between Pages/comps/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsComponent } from './collections/collections.component';
-import { ElementsComponent } from './elements/elements.component';
-import { ModsComponent } from './mods/mods.component';
-import { ViewsComponent } from './views/views.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CollectionsComponent,
-    ElementsComponent,
-    ModsComponent,
-    ViewsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
