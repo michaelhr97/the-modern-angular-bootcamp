108ffbca7d16c708408e3823345eb38b33d37e2c
michael <michaelhernandezrala@gmail.com>
2023-11-19 14:56:05 +0100

    Communicating down table data

--- 09 - Lazy Loading with Modules/comps/src/app/collections/table/table.component.ts
+++ 09 - Lazy Loading with Modules/comps/src/app/collections/table/table.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-
+  @Input() data: { name: string; age: number; job: string }[] = [];
+  @Input() headers: { key: string; label: string }[] = [];
 }
