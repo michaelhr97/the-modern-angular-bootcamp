c0e373da4d00aa333df6def762e49f4489fbbfeb
michael <michaelhernandezrala@gmail.com>
2023-11-19 15:14:16 +0100

    Set up project

---
+++ 10 - Advanced Component Routing/comps/src/app/collections/collections.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsRoutingModule } from './collections-routing.module';
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [CollectionsHomeComponent, TableComponent],
+  imports: [CommonModule, CollectionsRoutingModule, SharedModule],
+  exports: [CollectionsHomeComponent],
+})
+export class CollectionsModule {}
