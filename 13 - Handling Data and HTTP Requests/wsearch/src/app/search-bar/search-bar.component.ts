269db38632e3c6c491058d2108334d097059fa14
michael <michaelhernandezrala@gmail.com>
2023-11-21 19:59:51 +0100

    Component design methodology

--- 13 - Handling Data and HTTP Requests/wsearch/src/app/search-bar/search-bar.component.ts
+++ 13 - Handling Data and HTTP Requests/wsearch/src/app/search-bar/search-bar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  term = '';
 
+  onInput(target: any) {
+    this.term = target.value;
+  }
 }
